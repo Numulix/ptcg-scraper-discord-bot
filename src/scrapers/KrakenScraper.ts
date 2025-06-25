d9278dcdfc5764bec94fa885fc5fb3c11be8ddd7
Numulix <jbabic999@gmail.com>
2025-06-25 10:45:02 +0200

    fix(kraken): rewrite Kraken scraper in a more effective and stable way

--- src/scrapers/KrakenScraper.ts
+++ src/scrapers/KrakenScraper.ts
@@ -1,8 +1,7 @@
 import * as cheerio from 'cheerio';
-import { chromium } from "playwright";
-import { logger } from "../core/Logger.js";
 import { Product } from "../types/Product.js";
 import { Scraper } from "./Scraper.js";
+import axios from 'axios';
 
 export class KrakenScraper extends Scraper {
     public readonly storeName: string = "kraken";
@@ -11,72 +10,51 @@ export class KrakenScraper extends Scraper {
     public readonly logoUrl: string = "https://www.kraken.rs/wp-content/uploads/2022/12/kraken-logo-270x62-1.png";
 
     public async scrape(): Promise<Product[]> {
-        let browser = null;
+        const products: Product[] = [];
 
-        try {
-            browser = await chromium.launch();
-            const context = await browser.newContext();
-            const page = await context.newPage();
+        const selector = ".product-wrapper";
+        let page: number = 1;
+        let lastPage: boolean = false;
+        const getPageUrl = (pageNumber: number) => `${this.baseUrl}/kategorija/igre-sa-kartama/pokemon/page/${pageNumber}/?per_page=96&_pjax=.main-page-wrapper&loop=96&woo_ajax=1`
+        const htmlArray: string[] = [];
 
-            const selector = ".products.wd-products";
-            const loadingOnScrollSelector = "a.load-on-scroll";
+        while (!lastPage) {
 
-            await page.goto(
-                `${this.baseUrl}/kategorija/igre-sa-kartama/pokemon/?per_page=96&shop_view=grid&per_row=6`,
-                { waitUntil: 'networkidle' }
-            );
-
-            const maxClicks = 25;
-
-            for (let clickCount = 0; clickCount < maxClicks; clickCount++) {
-                const loadMoreButton = page.locator(loadingOnScrollSelector);
-
-                if (await loadMoreButton.count() > 0) {
-                    await loadMoreButton.click({ clickCount: 1 });
-                    await page.waitForLoadState('networkidle', { timeout: 10000 });
-                    await page.waitForSelector(`${loadingOnScrollSelector}.loading`, { state: 'detached', timeout: 10000 });
-                } else {
-                    logger.info("No more loading found, breaking")
+            try {
+                const response = await axios.get(getPageUrl(page));
+                htmlArray.push(response.data.items);
+                page++;
+            } catch (error) {
+                if (axios.isAxiosError(error) && error.response && error.response.status === 404) {
+                    lastPage = true;
                     break;
                 }
-
-                await page.waitForTimeout(1000);
-            }
-
-            const html = await page.content();
-            const $ = cheerio.load(html);
-            const productCards = $(selector).children("div");
-
-            const products: Product[] = [];
-
-            productCards.each((i, element) => {
-                const productImageLink = $(element).find(".product-image-link");
-                const url = $(productImageLink).attr("href");
-                const imageUrl = $(productImageLink).find("img").attr("src");
-                const name = $(element).find(".wd-entities-title").find("a").text();
-                const price = $(element).find("bdi").text();
-                const inStock = $(element).find(".out-of-stock").length === 0;
-
-                products.push({
-                    storeName: "kraken",
-                    name,
-                    price,
-                    url: url || "",
-                    imageUrl,
-                    inStock
-                })
-            })
-
-            return products;
-        } catch (error) {
-            logger.error(`Error scraping: ${this.storeName} ${error}`);
-            return [];
-        } finally {
-            if (browser) {
-                browser.close();
             }
         }
+
+        const html = htmlArray.join('');
+        const $ = cheerio.load(html);
+        const productCards = $(selector);
+
+        productCards.each((i, element) => {
+            const productImageLink = $(element).find(".product-image-link");
+            const url = $(productImageLink).attr("href");
+            const imageUrl = $(productImageLink).find("img").attr("src");
+            const name = $(element).find(".wd-entities-title").find("a").text();
+            const price = $(element).find("bdi").text();
+            const inStock = $(element).find(".out-of-stock").length === 0;
+
+            products.push({
+                storeName: "kraken",
+                name,
+                price,
+                url: url || "",
+                imageUrl,
+                inStock
+            })
+            
+        })
+
+        return products;
     }
 }
-
-new KrakenScraper().scrape();
